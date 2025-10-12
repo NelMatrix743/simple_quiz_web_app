70d92441bcd2a9c4ab23d43e291c0f5aa5921063
NelMatrix743 <nelmatrix155@gmail.com>
2025-10-12 02:46:58 +0000

    implement question and answer display functionalities

--- script.js
+++ script.js
@@ -7,6 +7,13 @@
  */
 
 
+// map HTML elements to JS DOM objects
+const questionComponent = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextQuestionButton = document.getElementById("nxt-btn");
+const scoreNumElement = document.getElementById("score-num");
+
+
 // implement 10 CS questions
 const questionLists =  [
   {
@@ -102,27 +109,47 @@ const questionLists =  [
 ];
 
 
-// map HTML elements to JS DOM objects
-const questionComponent = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextQuestionButton = document.getElementById("nxt-btn");
-const scoreNumElement = document.getElementById("score-num");
+function displayQuestion(currentQuestionIndex){
+  // render question
+  let currentQuestion = questionLists[currentQuestionIndex];
+  let questionNumber = currentQuestionIndex + 1;
+  questionComponent.innerHTML = `${questionNumber}. ${currentQuestion.question}`;
 
+  // clear previous answers
+  answerButtons.innerHTML = "";
+
+  // render answers
+  currentQuestion.answers.forEach(answer => {
+    const optionButton = document.createElement("button");
+    optionButton.classList.add("btn");
+    optionButton.innerHTML = answer.option;
+    answerButtons.appendChild(optionButton);
+  });
+}
+
+
+function startQuiz(currentQuestionIdx, score){
+  nextQuestionButton.style.display = "block";
+  nextQuestionButton.innerHTML = "NEXT QUESTION";
+  displayQuestion(currentQuestionIdx);
+}
 
 
 // main section
 
-let currentQuestionIndex;
-let currentScore;
+let currentQuestionIndex = 0;
+let currentScore = 0;
 
-let progress = sessionStorage.getItem("QUIZ_IN_PROGRESS");
+// let progress = sessionStorage.getItem("QUIZ_IN_PROGRESS");
 
-if(!Boolean(progress)){
-  sessionStorage.setItem("QUIZ_IN_PROGRESS", !Boolean(progress));
-  currentQuestionIndex = 0;
-  currentScore = 0;
-} else {
-  currentQuestionIndex = sessionStorage.getItem("CURRENT_QUESTION");
-  currentScore = sessionStorage.getItem("CURRENT_SCORE");
-}
+// if(!Boolean(progress)){
+//   sessionStorage.setItem("QUIZ_IN_PROGRESS", !Boolean(progress));
+//   currentQuestionIndex = 0;
+//   currentScore = 0;
+// } else {
+//   currentQuestionIndex = sessionStorage.getItem("CURRENT_QUESTION");
+//   currentScore = sessionStorage.getItem("CURRENT_SCORE");
+// }
 
+
+startQuiz(currentQuestionIndex, currentScore);
